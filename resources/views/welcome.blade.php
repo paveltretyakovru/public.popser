5c091a7029e22c72eaa42e4c73f33938ee7efa25
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2016-01-13 10:15:59 +0300

    Change title tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Popser! Расширения для Chrome, ведение учета сериалов</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato|Helvetica|Arial&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
 
